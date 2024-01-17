0fe0b3ceab1bfe848177fa63aa99c3d3086ff77f
Kevin Levasseur <kevin_levasseur@telus.net>
2024-01-17 14:42:57 -0800

    initial commit

---
+++ asyncBreeds.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, done) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+    if (!error) done(data);
+  });
+};
+
+const callback = breed => {
+  console.log('Return Value: ', breed);
+};
+
+const bombay = breedDetailsFromFile('Bombay', callback);
